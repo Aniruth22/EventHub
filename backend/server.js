27c97cb34185952bf3590167d17aad1fc05ebbc0
Aniruth22 <aniruthredddy22@gmail.com>
2026-04-15 03:24:59 +0530

    fixed backend errors and conflicts

--- backend/server.js
+++ backend/server.js
@@ -45,7 +45,7 @@ app.get('/', (req, res) => {
 });
 
 // Define the port the server will run on, from .env file or default to 5000
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 // Start the server and listen for incoming requests
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
