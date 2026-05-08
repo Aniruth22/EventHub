7094cdade885322362fd2266c24c2a5bb9badbd8
Aniruth22 <aniruthredddy22@gmail.com>
2026-05-08 16:54:35 +0530

    fixed Event model import for render linux

--- backend/routes/events.js
+++ backend/routes/events.js
@@ -1,6 +1,6 @@
   const express = require('express');
   const router = express.Router();
-  const Event = require('../models/Event');
+  const Event = require('../models/Events');
   const User = require('../models/User');
   const auth = require('../middleware/authMiddleware');
   const checkRole = require('../middleware/roleMiddleware');
